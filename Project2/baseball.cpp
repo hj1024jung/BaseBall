78f23789442fd8c9a7f9b4876f574c227c5c7964
hj1024.jung <flksignal@gmail.com>
2023-07-05 16:57:28 +0900

    feat. page27

--- Project2/baseball.cpp
+++ Project2/baseball.cpp
@@ -1,8 +1,12 @@
-#include <vector>
+#include <stdexcept>
 
 using namespace std;
 
 class BaseBall
 {
 public:
+	void guess(const string& string)
+	{
+		throw length_error("Must be three letters.");
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,9 @@ TEST(TestSuiteBase, AlwaysPass)
 {
 	EXPECT_EQ(1, 1);
 }
+
+TEST(TestBaseballGame, ThrowExceptionWhenInputLengthIsUnmatched)
+{
+	BaseBall game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
