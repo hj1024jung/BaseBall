6cb994aebd27553a0a75229aac5df89f7d9405c2
hj1024.jung <flksignal@gmail.com>
2023-07-05 17:01:25 +0900

    feat. page30

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -11,3 +11,11 @@ TEST(TestBaseballGame, ThrowExceptionWhenInputLengthIsUnmatched)
 	BaseBall game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(TestBaseballGame, ThrowExceptionWhenInvalidChar)
+{
+	BaseBall game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+	EXPECT_THROW(game.guess(string("1s2")), invalid_argument);
+	EXPECT_THROW(game.guess(string("s12")), invalid_argument);
+}
